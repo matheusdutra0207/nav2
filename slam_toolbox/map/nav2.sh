40724afa2575137e9b1da36b987743100f2b0c74
Matheus Dutra <matheusdutra0207@gmail.com>
2024-01-31 09:20:06 -0300

    adding ekf params file

---
+++ slam_toolbox/map/nav2.sh
@@ -0,0 +1,43 @@
+#Edited from https://answers.ros.org/question/300113/docker-how-to-use-rviz-and-gazebo-from-a-container/
+# https://docs.ros.org/en/humble/How-To-Guides/Run-2-nodes-in-single-or-separate-docker-containers.html
+# sudo bash ros2-humble-docker.sh
+# If not working, first do: sudo rm -rf /tmp/.docker.xauth
+# It still not working, try running the script as root.
+
+XAUTH=/tmp/.docker.xauth
+
+echo "Preparing Xauthority data..."
+xauth_list=$(xauth nlist :0 | tail -n 1 | sed -e 's/^..../ffff/')
+if [ ! -f $XAUTH ]; then
+    if [ ! -z "$xauth_list" ]; then
+        echo $xauth_list | xauth -f $XAUTH nmerge -
+    else
+        touch $XAUTH
+    fi
+    chmod a+r $XAUTH
+fi
+
+echo "Done."
+echo ""
+echo "Verifying file contents:"
+file $XAUTH
+echo "--> It should say \"X11 Xauthority data\"."
+echo ""
+echo "Permissions:"
+ls -FAlh $XAUTH
+echo ""
+echo "Running docker..."
+docker run -it\
+    --rm\
+    --env="DISPLAY=$DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --env="XAUTHORITY=$XAUTH" \
+    --volume="$XAUTH:$XAUTH" \
+    --net=host \
+    --privileged \
+    --name=ros-humble-rviz \
+    -t nav2 \
+    bash
+echo "Done."
+
